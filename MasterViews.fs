906866e7cc6038763848dac98c570236808cdc36
Michael Doffing <43443159+cmdoffing@users.noreply.github.com>
2025-04-28 22:21:41 -0400

    Update MasterViews.fs
    Added [ to meta expression.

--- MasterViews.fs
+++ MasterViews.fs
@@ -35,10 +35,10 @@ let navView =
     ]
 
 let headHtml =
-    head [] [
+    head []  [
         title  [] [Text "Genesys"]
         meta   [_charset "utf-8"]
-        meta    _name "viewport"; _content "width=device-width, initial-scale=1"]
+        meta   [_name "viewport"; _content "width=device-width, initial-scale=1"]
         link   [_href "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"; _rel "stylesheet"]
         script [_src "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"]  []
     ]
